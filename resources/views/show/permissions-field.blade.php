81535aff95dc985bdff75941fe7a01f973d80af1
Nathan Jansen <info@nathanjansen.nl>
2020-12-16 16:17:03 +0100

    Removed icons

--- resources/views/show/permissions-field.blade.php
+++ resources/views/show/permissions-field.blade.php
@@ -5,9 +5,14 @@
         @foreach ($options as $option)
             <div class="flex">
                 @if ($field->resource->permissions->contains('name', $option->name))
-                    <x-ri-checkbox-blank-circle-fill title="{{ __($option->name) }}" class="h-4 w-4 mr-1 mt-1 last:mr-0 text-green-500" />
+
+                    <svg class="fill-current h-4 w-4 mr-1 mt-1 last:mr-0 text-green-500" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" title="{{ __($option->name) }}">
+                        <path d="M512 512m-426.666667 0a426.666667 426.666667 0 1 0 853.333334 0 426.666667 426.666667 0 1 0-853.333334 0Z"  />
+                    </svg>
                 @else
-                    <x-ri-checkbox-blank-circle-fill title="{{ __($option->name) }}" class="h-4 w-4 mr-1 mt-1 last:mr-0 text-red-500" />
+                    <svg class="fill-current h-4 w-4 mr-1 mt-1 last:mr-0 text-red-500" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" title="{{ __($option->name) }}">
+                        <path d="M512 512m-426.666667 0a426.666667 426.666667 0 1 0 853.333334 0 426.666667 426.666667 0 1 0-853.333334 0Z"  />
+                    </svg>
                 @endif
                 <span class="pl-2">{{ ucfirst($option->name) }}</span>
             </div>
